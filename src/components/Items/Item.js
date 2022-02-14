ad6ad4d040ea8d14adeb8ecab8025cc0b9def701
Rodrigo Lopez <rodrigoiglopez@gmail.com>
2022-02-13 22:46:32 -0300

    se agrega linkeado al detalle de producto

--- src/components/Items/Item.js
+++ src/components/Items/Item.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 export const Item = ({id, title, description, price, imgUrl}) => {
   return (
     <>
-      <div className="card">
-        <h2>{title}</h2>
-        <h3>{description}</h3>
-        <p>${price}</p>
-      </div>
+      <Link to={`/producto/${id}`}>
+        <div className="card">
+          <h2>{title}</h2>
+          <h3>{description}</h3>
+          <p>${price}</p>
+        </div>
+      </Link>
     </>
   );
 }
