533b9b6bf55778541832fa7a5cb8f1b4554cd127
Rodrigo Lopez <rodrigoiglopez@gmail.com>
2022-02-27 22:22:35 -0300

    se borran archivos y funciones sin utilizar

--- src/Helpers/GetData.js
+++ src/Helpers/GetData.js
@@ -1,30 +1,5 @@
-import { Products } from "../Mocks/Products";
 import { db } from "../firebase/config";
 import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
-
-export const GetProductList2  = (category) => {
-  return new Promise( (resolve, reject) => {
-    setTimeout(() => {
-
-      if (category)
-      {
-        resolve(Products.filter(prod => prod.category === category));
-      }
-      else
-      {
-        resolve(Products);
-      }
-    }, 500);
-  });
-}
-
-export const GetProductDetail2 = (productId) => {
-  return new Promise ((resolve) => {
-    setTimeout(() => {
-      resolve(Products.filter(prod => prod.id === productId));
-    }, 500)
-  });
-}
 
 export const GetProductList = (category) => {
 
